6e7116b0f6e3a835fa5a5be36f08940b34e23ef5
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2023-09-17 01:51:04 +0800

    Update JavDB-AD.js

--- JavDB-AD.js
+++ JavDB-AD.js
@@ -1,10 +1,5 @@
-/*************************************
+/*
 项目名称：JavDB-去广告
-下载地址：https://javdb008.com
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
-使用声明：⚠️仅供参考，🈲转载与售卖！
-**************************************
 
 [rewrite_local]
 ^https?:\/\/api\.((pxxgg|yijingluowangluo)\.xyz|ujvnmkx\.cn)\/api url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/Javbd-AD.js
@@ -12,28 +7,28 @@
 [mitm]
 hostname = api.pxxgg.xyz, api.ujvnmkx.cn, api.yijingluowangluo.xyz
 
-*************************************/
+*/
 
 
 var body = $response.body;
-var Tloml.gm = JSON.parse(body);
+var Tlomlgm = JSON.parse(body);
 
 const ada = '/ads';
 const adb = '/startup';
 
 // 横幅广告
 if ($request.url.indexOf(ada) != -1){
-  Tloml.gm.data.ads = {};
+  Tlomlgm.data.ads = {};
 }
 
 // 公告，开屏
 if ($request.url.indexOf(adb) != -1){
-  Tloml.gm.data.splash_ad.enabled = false;
-  Tloml.gm.data.splash_ad.overtime = 0;
-  Tloml.gm.data.splash_ad.ad = {};
-  Tloml.gm.data.feedback.placeholder = "";
-  Tloml.gm.data.settings.UPDATE_DESCRIPTION = "";
-  Tloml.gm.data.settings.NOTICE = "";
+  Tlomlgm.data.splash_ad.enabled = false;
+  Tlomlgm.data.splash_ad.overtime = 0;
+  Tlomlgm.data.splash_ad.ad = {};
+  Tlomlgm.data.feedback.placeholder = "";
+  Tlomlgm.data.settings.UPDATE_DESCRIPTION = "";
+  Tlomlgm.data.settings.NOTICE = "";
 }
 
-$done({ body: JSON.stringify(Tloml.gm) });
+$done({ body: JSON.stringify(Tlomlgm) });
