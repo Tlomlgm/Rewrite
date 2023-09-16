43f4b40ef958180df940dce8648f49d2262a4f8d
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2023-09-17 01:36:54 +0800

    Update Nivod4-AD.js

--- Nivod4-AD.js
+++ Nivod4-AD.js
@@ -17,7 +17,7 @@
 
 [rewrite_local]
 
-^https?:\/\/.*nivod.*\/($|[0-9a-zA-Z=_/-]+\.html) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/nivod.js
+^https?:\/\/.*nivod.*\/($|[0-9a-zA-Z=_/-]+\.html) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/Nivod4-AD.js
 
 [mitm]
 
