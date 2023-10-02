536f21658ede975b81f2f8a6e9ad44dd64884a02
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2023-10-03 01:50:26 +0800

    Create Nicegram.js

---
+++ Nicegram.js
@@ -0,0 +1,16 @@
+/*
+
+#Nicegram
+[rewrite_local]
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/Nicegram.js
+
+[mitm] 
+hostname = restore-access.indream.app
+
+*/
+
+{
+  "data" : {
+    "premiumAccess" : true
+  }
+}
