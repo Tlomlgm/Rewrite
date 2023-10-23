bfde8ae45ea117b6230c399bb6f869faf5795c2e
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2023-10-23 13:21:47 +0800

    Update JavDB.js

--- JavDB.js
+++ JavDB.js
@@ -2,9 +2,9 @@
 
 [rewrite_local]
 
-^https:\/\/(api\.(yijingluowangluo|jdforrepam|hechuangxinxi)\.xyz)\/api\/(v1\/(ads|startup|users)|v4\/(movies|plans)|v4/\w+) url script-response-body https://raw.githubusercontent.com/Tlomlgm/Cite/main/JavDB-DaTa.js
+^https:\/\/(api\.(yijingluowangluo|hechuangxinxi|jdforrepam)\.xyz|jdforrepam\.com)\/api\/(v1\/(ads|startup|users)|v4\/(movies|plans)\/\w+) url script-response-body https://raw.githubusercontent。com/Tlomlgm/Cite/main/JavDB-DaTa.js
 
-^https?:\/\/api\.((pxxgg|hechuangxinxi|yijingluowangluo)\.xyz|ujvnmkx\.cn)\/api\/v1\/movies\/\w+\/play\? url script-request-header https://raw.githubusercontent.com/Tlomlgm/Cite/main/JavDB_DaTa.js
+^https:\/\/(api\.(yijingluowangluo|hechuangxinxi)\.xyz|jdforrepam\.com)\/api\/v1/movies\/\w+\/play\? url script-request-header https://raw.githubusercontent.com/Tlomlgm/Cite/main/JavDB_DaTa.js
 
 
 [mitm]
