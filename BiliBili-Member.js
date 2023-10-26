6fbe745889832dd41d908f0943ad66dbba74c801
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2023-10-26 16:43:15 +0800

    Delete BiliBili-Member.js

--- BiliBili-Member.js
+++
@@ -1,8 +0,0 @@
-#哔哩哔哩会员
-
-[rewrite_local]
-
-^https?:\/\/ap(i|p)\.bilibili\.com\/bilibili(.*)\/(View|Play(URL|View|Conf|erOnline)|MainList|ViewProgress)$* url script-response-body https://raw.githubusercontent.com/ClydeTime/Surge/main/Script/BiliBili/biliCrack.js
-
-[mitm]
-hostname = app.bilibili.com
