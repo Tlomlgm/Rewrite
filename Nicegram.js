3f354bee244a977c6614a632528e2a3a3a02a263
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2023-11-11 14:17:09 +0800

    Update Nicegram.js

--- Nicegram.js
+++ Nicegram.js
@@ -1,6 +1,7 @@
 /*
+#Nicegram v1.1.1
+https://apps.apple.com/us/app/nicegram-secure-telegram-chat/id1608870673?l=zh-Hans-CN
 
-#Nicegram
 [rewrite_local]
 ^https?:\/\/restore-access\.indream\.app\/restoreAccess url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/Nicegram.js
 
