bf9f5341b6df08dfc89755c6e48a1dfc95064305
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2023-11-11 14:09:39 +0800

    Update AoRuan-Series.js

--- AoRuan-Series.js
+++ AoRuan-Series.js
@@ -1,5 +1,4 @@
-/*************************************
-
+/*
 项目名称：傲软抠图
 下载地址：https://t.cn/A6xBOE5d
 项目名称：傲软扫描
@@ -24,18 +23,13 @@
 电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
-**************************************
-
 [rewrite_local]
-
 ^https?:\/\/.*\.(aoscdn\.com|apsapp\.cn) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/arqjt.js
 
 [mitm] 
-
 hostname = *.aoscdn.com, *.apsapp.cn
 
-*************************************/
-
+*/
 
 var chxm1023 = JSON.parse($response.body);
 const vipa = '/base/vip/client/authorizations';
