aa746e5096eac4d7cde537d8fd9b38196e4189e9
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2023-11-11 14:21:23 +0800

    Update PsExpress.js

--- PsExpress.js
+++ PsExpress.js
@@ -1,8 +1,8 @@
 /*
-            
-# Ps Express
-[rewrite_local]
+#Ps Express v23.30.0
+https://apps.apple.com/cn/app/photoshop-express-%E5%9B%BE%E7%89%87%E7%BC%96%E8%BE%91-%E4%BF%AE%E5%9B%BE/id331975235
 
+[rewrite_local]
 ^https:\/\/lcs-mobile-cops\.adobe\.io\/mobile_profile url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/PsExpress.js
 
 [mitm] 
