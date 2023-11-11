66b44544563db719a37a67b5a477b7df8da80d98
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2023-11-11 14:10:14 +0800

    Update BiliBili_Skin.js

--- BiliBili_Skin.js
+++ BiliBili_Skin.js
@@ -1,7 +1,6 @@
 #哔哩哔哩会员皮肤
 
 [rewrite_local]
-
 ^https?:\/\/ap(i|p)\.bilibili\.com\/x\/(vip|v2|resource)\/(space|account|web|price|top_panel_info|show)(\/|\?)(mine|myinfo|access|vip_center|panel|_bridge|skin\?).* url script-response-body https://raw.githubusercontent.com/ClydeTime/Surge/main/Script/BiliBili/biliSkin.js
 
 [mitm]
