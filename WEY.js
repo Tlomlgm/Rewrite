df7a8e0920f5df22d24f93effd3748c022f7bb56
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-01-05 11:00:02 +0800

    Create WEY.js

---
+++ WEY.js
@@ -0,0 +1,25 @@
+/*
+魏 去掉 发现/商城/
+
+[rewrite_local]
+^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/.+\/content\/route\/getWholeNodeContentInfo url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/WEY.js
+
+[mitm]
+hostname = gw-app.beantechyun.com
+
+*/
+
+var Tlomlgm = JSON.parse($response.body);
+
+for (var i = 0; i < Tlomlgm.data.length; i++) {
+  Tlomlgm.data[i].contentMessageList = Tlomlgm.data[i].contentMessageList.filter(item => item.title !== '发现' && item.title !== '商城');
+  
+  let indexA = Tlomlgm.data[i].contentMessageList.findIndex(item => item.title === '爱车');
+  let indexB = Tlomlgm.data[i].contentMessageList.findIndex(item => item.title === '服务');
+
+  if (indexA !== -1 && indexB !== -1) {
+    [Tlomlgm.data[i].contentMessageList[indexA], Tlomlgm.data[i].contentMessageList[indexB]] = [Tlomlgmo.data[i].contentMessageList[indexB], Tlomlgmo.data[i].contentMessageList[indexA]];
+  }
+}
+
+$done({body: JSON.stringify(Tlomlgmo)});
