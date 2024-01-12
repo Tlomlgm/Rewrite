25974596fbaccd6604449cf89e050bf440a29400
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-01-13 01:44:05 +0800

    Update WEY.js

--- WEY.js
+++ WEY.js
@@ -1,25 +1,57 @@
 /*
-魏 
-去掉 开屏/发现/商城/轮播/广告
+WEY 
 
 [rewrite_local]
+#个人界面
+^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v1\.0\/userAuth\/route\/getUserInfo url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/WEY.js
+#车体图 型号
+^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v2\.0\/vehicle\/acquireVehicles url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/WEY.js
+#会员积分
+^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v1\.0\/point\/querySumPoint url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/WEY.js
+#优化底栏+净化AD
 ^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/(wey\/.+|.+)\/content\/route\/(getWholeNodeContentInfo\?contentType=(MENU|APPSECONDAD)|getContentInfo) url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/WEY.js
 
 [mitm]
 hostname = gw-app.beantechyun.com
 
 */
-
-var Tlomlgm = JSON.parse($response.body);
+var WEY = JSON.parse($response.body);
 const Alter = /contentType=MENU/;
 const AD = /(getContentInfo|contentType=APPSECONDAD)/;
+const My = /getUserInfo/;
+const Car = /acquireVehicles/;
+const huiyyuan = /querySumPoint/;
 
-if(Alter.test($request.url)){
-  for (var i = 0; i < Tlomlgm.data.length; i++) {  Tlomlgm.data[i].contentMessageList = Tlomlgm.data[i].contentMessageList.filter(item => item.title !== '发现' && item.title !== '商城');}
+if (Alter.test($request.url)) {
+    for (var i = 0; i < WEY.data.length; i++) {
+        WEY.data[i].contentMessageList = WEY.data[i].contentMessageList.filter(item => item.title !== '发现' && item.title !== '商城');
+    }
 }
 
-if(AD.test($request.url)){
-  Tlomlgm.data = [];
+if (AD.test($request.url)) {
+    WEY.data = [];
 }
 
-$done({body: JSON.stringify(Tlomlgm)});
+if (My.test($request.url)) {
+    WEY.data.showBrandName = "GT PHEV ";
+    WEY.data.joinBoardName = "永臻分会";//个人分会名字
+    WEY.data.numberOfSubscribed = 99999;//个人订阅
+    WEY.data.threadPraisedNumber = "99999";//个人获赞
+    WEY.data.likeOtherThreadNumber = 99999;//我的点赞
+    WEY.data.collectNumber = "99999";//我的收藏
+    WEY.data.concernNumber = 99999;//我的关注
+    WEY.data.fansNumber = 99999;//我的粉丝
+    WEY.data.replyNumber = 99999;//我的评论
+    WEY.data.levelCode = "wvip8";//VIP等级
+}
+
+if (huiyyuan.test($request.url)) {
+    WEY.data.remindPoint = 999999999;
+    WEY.data.totalPoint = 999999999;
+}
+
+if (Car.test($request.url)) {
+    $response.body = $response.body.replace(/"scmsModelName\":"VV7"/g, '"scmsModelName":"VV7 GT PHEV"');
+}
+
+$done({ body: JSON.stringify(WEY) });
