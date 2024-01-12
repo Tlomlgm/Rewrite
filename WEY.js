33fca93adf4e4c23fb2b7afed518348100560b48
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-01-13 02:17:13 +0800

    Update WEY.js

--- WEY.js
+++ WEY.js
@@ -6,6 +6,7 @@ WEY
 ^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v1\.0\/userAuth\/route\/getUserInfo url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/WEY.js
 
 #车体图 型号
+^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v2\.0\/vehicle\/acquireVehicles url response-body "material90Url":"https://gw-tsp-file.beantechyun.com/files/bt-master-vehicle-service53826251641039585车体图_20220629_WEY-VV7/WEY-VV7_炫晶黑/90.png" response-body "material90Url":"https://s2.loli.net/2024/01/09/wkALidNrSXhJnzv.png"
 ^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v2\.0\/vehicle url response-body "scmsModelName":"VV7" response-body "scmsModelName":"VV7 GT PHEV"
 
 #会员积分
