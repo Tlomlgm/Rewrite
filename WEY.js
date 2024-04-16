295387bace1e4371820dbcc9e5b6e28e8196ac54
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-04-16 19:31:50 +0800

    Update WEY.js

--- WEY.js
+++ WEY.js
@@ -6,9 +6,6 @@ WEY
 ^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v1\.0\/userAuth\/route\/getUserInfo url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/WEY.js
 ^https:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v1\.0\/complaintsComments\/getCollectCount url response-body "data":0 response-body "data":99999
 
-#车模型
-^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v2\.0\/vehicle url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/WEY.js
-
 #会员积分
 ^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v1\.0\/point\/querySumPoint url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/WEY.js
 
@@ -23,8 +20,7 @@ var WEY = JSON.parse($response.body);
 const Alter = /contentType=MENU/;
 const AD = /(getContentInfo|contentType=APPSECONDAD)/;
 const My = /getUserInfo/;
-const huiyuan = /querySumPoint/;
-const moxing = /vehicle/;
+const huiyyuan = /querySumPoint/;
 
 if (Alter.test($request.url)) {
     for (var i = 0; i < WEY.data.length; i++) {
@@ -50,13 +46,9 @@ if (My.test($request.url)) {
     WEY.data.levelCode = "wvip8";//VIP等级
 }
 
-if (huiyuan.test($request.url)) {
+if (huiyyuan.test($request.url)) {
     WEY.data.remindPoint = 999999999999;
     WEY.data.totalPoint = 999999999999;
-}
-
-if (moxing.test($request.url)) {
-    WEY.data.material90Url = https://s2.loli.net/2024/01/09/wkALidNrSXhJnzv.png;
 }
 
 $done({ body: JSON.stringify(WEY) });
