968d0b873f256ef803177c43839afb9d379e5ff2
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-07-18 00:18:47 +0800

    Update Revenuecat.js

--- Revenuecat.js
+++ Revenuecat.js
@@ -1,15 +1,51 @@
-/*
+/*************************************
+
+项目名称：revenuecat合集 （此合集只包含10.5及以后的软件）
+软件版本：均适配2023.9.8最新 （每一个下载地址对应一个UAMappings）
+下载地址：星锤日记 https://is.gd/R5KqD4
+下载地址：倒数鸭  https://is.gd/rETAhp
+下载地址：星垂专注 https://is.gd/rEG9H5
+下载地址：Context https://is.gd/splCnF
+下载地址： Vision-个人OKR目标管理 https://t.cn/A6OxXNxK
+下载地址：Structured-每日计划 https://t.cn/A6cWhz4X
+下载地址：cookie记账 
+下载地址：倒数鸭 
+下载地址：HTTPBOT 2022.2.1 作者zoo
+下载地址：Mypianist 2.08
+下载地址：TouchRetouch 5.1.12
+下载地址：Appspree https://t.cn/A6otfeAc 3.1
+下载地址：Persona 1.824
+下载地址：WordSwag 4.56
+下载地址：AnkiPro 1.22.1
+下载地址：SmartAI 
+下载地址：AI Chat 
+下载地址：‎AI Type
+下载地址：TextMask
+下载地址：Music Mate
+下载地址：Langster https://too.st/7aW 同作者5个软件 
+下载地址：muse 同作者2个软件
+下载地址：Funexpected 
+下载地址：中国法律
+使用声明：⚠️仅供参考，🈲️转载与售卖！
+
+**************************************
 
 [rewrite_local]
 #修改
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/Revenuecat.js
+^https?:\/\/api-\paywalls.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/Revenuecat.js
+
+^https?:\/\/api-\paywalls.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/Revenuecat.js
+
 #清理
+^https?:\/\/api-\paywalls.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/Revenuecat.js
+
 ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/Revenuecat.js
 
-[mitm] 
-hostname = api.revenuecat.com
 
-*/
+[mitm] 
+hostname = api.revenuecat.com,api-paywalls.revenuecat.com
+
+************************************/
 
 const Q = {};
 const Q1 = JSON.parse(typeof $response != "undefined" && $response.body || null);
@@ -32,13 +68,17 @@ if (typeof $response == "undefined") {
   var UA = $request.headers['user-agent'];
   const app = '1';
   const UAMappings = {
+    'Python3IDE':{ name: 'pro', id: 'python3ide_six_month'},//7.14
+    'Scale%20Finder':{ name: 'Pro', id: 'sf_2999_1y_1w0'},//7.14
+    'Who%20Stalks':{ name: 'Premium', id: 'ws_999_1m'},//7.14
+    'PrevisShot':{ name: 'VIP', id: 'com.previsshot.previsshot.continuous_subscribe_12 个月之前_vip'},//7.9
     'Super%20AI%20Chat':{ name: 'premium', id: 'chatbot_v4_1999_1y'},
     'MusicPutty':{ name: 'pro_version', id: 'mp_3599_1y'},//6.24
     'Linearity':{ name: 'pro', id: 'linearity_curve_pro_yearly_launch_offer'},
     'iplayTV':{ name: 'com.ll.btplayer.12', id: 'com.ll.btplayer.12'},//6.21
     '%E8%B5%84%E6%BA%90%E6%90%AC%E8%BF%90%E5%A4%A7%E5%B8%88':{ name: 'SaveTikYoutu_common', id: 'LifetimeSubscription'},
     'DHWaterMarkManager':{ name: 'Vip', id: 'lifetimeVIP_001'},//6.20
-    
+    'Krishna%20VPN%20Plus%20Browser':{ name: 'vpnz-lifetime', id: 'vpnz-lifetime'},//7.7
     'XBListeningEnglish':{ name: 'enPro', id: 'com.shenming.newconceptvip.year'},
     'FretTrainer':{ name: 'pro', id: 'frettrainer.sub.yearly.pro'},//5.7
     '%E9%B2%B8%E8%90%BD%E6%96%87%E6%A1%88':{ name: 'vip', id: 'jl_year'},//2024.5.6
@@ -93,9 +133,9 @@ if (typeof $response == "undefined") {
     'windiary':{ name: 'Pro', id: 'windiary_1799_lt'},
     'Liftbear':{ name: 'Pro', id: 'liftbear_2399_1y'},
     'Currency':{ name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.pro.crossgrade'},
-    
-    
-    
+    
+    
+    
     };
 
   const data = {
