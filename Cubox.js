b630a221d496f4a88987c6afb8f90260d5791d24
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-07-20 14:21:45 +0800

    Update Cubox.js

--- Cubox.js
+++ Cubox.js
@@ -1,1 +1,29 @@
+/*
+#Cubox
+[rewrite_local]
 
+^https:\/\/cubox\.(pro|cc)\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/Cubox.js
+
+[mitm]
+hostname = cubox.*
+
+*/
+
+var body = $response.body;
+var url = $request.url;
+var Tlomlgm = JSON.parse(body);
+const vip = '/userInfo';
+
+if (url.indexOf(vip) != -1) {
+    Tlomlgm.data.level = 1;
+    Tlomlgm.data.expireTime = "2099-09-09T09:09:09+09:09";
+    Tlomlgm.data.nickName = "Tlomlgm";
+    Tlomlgm.data.thirdNickName = "Tlomlgm";
+    Tlomlgm.data.isExpire = false;
+    Tlomlgm.data.active = true;
+    Tlomlgm.data.isThirdUser = true;
+    Tlomlgm.data.payTime = 1660006006;
+    body = JSON.stringify(Tlomlgm);
+}
+
+$done({body});
