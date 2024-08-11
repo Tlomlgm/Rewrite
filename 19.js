3c902b8d4ef51e725f023d6c93672fbdfd5c2426
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-08-11 11:35:45 +0800

    Create 19.js

---
+++ 19.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+^https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-response-body https://raw.githubusercontent.com/Tlomlgm/Rewrite/main/91.js
+
+[mitm]
+hostname =*cloudfront.net,*ninghaixin.club,*wbwxe.com,wbapi.wbwxe.com,d13dw8kzjnavm.cloudfront.net,hxnd.*.vip
