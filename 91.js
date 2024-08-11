774d342a44d4cf3ccd0036f0a573e4ed4703a335
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-08-11 12:02:11 +0800

    Update 91.js

--- 91.js
+++ 91.js
@@ -2,4 +2,4 @@
 ^https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/Tlomlgm/Cite/main/91.js
 
 [mitm]
-hostname =*cloudfront.net,*ninghaixin.club,*wbwxe.com,wbapi.wbwxe.com,d13dw8kzjnavm.cloudfront.net,hxnd.*.vip
+hostname =*.cloudfront.net,*.ninghaixin.club,*wbwxe.com,wbapi.wbwxe.com,d13dw8kzjnavm.cloudfront.net,hxnd.*.vip
