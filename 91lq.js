bdae23e70d4d721f155350224ce33d35d4ceb9ee
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-08-19 01:19:08 +0800

    Delete 91lq.js

--- 91lq.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-^https:\/\/[^\s]+\.m3u8ex\/[^\s]+\?token=[^\s]+$ url script-request-header https://raw.githubusercontent.com/Tlomlgm/Cite/main/91lq.js
-
-[mitm]
-hostname = aawsdwd.iimwcb0j5.cc
