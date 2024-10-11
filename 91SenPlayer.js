94a457a043f1a32031b28863ce0ecf5604a38617
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-10-11 20:12:53 +0800

    更新 91SenPlayer.js

--- 91SenPlayer.js
+++ 91SenPlayer.js
@@ -1,5 +1,10 @@
+/*
+91系列视频
+
 [rewrite_local]
-^https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/Tlomlgm/Cite/main/91SenPlayer.js
+
+https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/Tlomlgm/Cite/refs/heads/main/91SenPlayer.js
 
 [mitm]
-hostname =*.cloudfront.net,*.ninghaixin.club,*.wbwxe.com,wbapi.wbwxe.com,d13dw8kzjnavm.cloudfront.net,hxnd.*.vip
+hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip
+*/
