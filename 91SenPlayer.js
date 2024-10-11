35420ad40c37fe63a034cae4ab1d90057c822d45
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-10-11 14:52:18 +0800

    Create 91SenPlayer.js

---
+++ 91SenPlayer.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+^https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/Tlomlgm/Cite/main/91SenPlayer.js
+
+[mitm]
+hostname =*.cloudfront.net,*.ninghaixin.club,*.wbwxe.com,wbapi.wbwxe.com,d13dw8kzjnavm.cloudfront.net,hxnd.*.vip
